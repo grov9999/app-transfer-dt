a6776d1a5b468d9a5ffdd464fed55598959d645c
Borisfc23 <borisfc23@gmail.com>
2025-03-31 23:51:52 -0500

    update form

--- app-transfer/src/components/atom/TextArea.tsx
+++ app-transfer/src/components/atom/TextArea.tsx
@@ -1,23 +1,36 @@
-import { DetalleTransferencia } from "../../interfaces/DetalleTransferencia"
+import { DetalleTransferencia } from "../../interfaces/DetalleTransferencia";
 
 interface TextAreaProps {
-    isDisable?: boolean,
-    tipo: string,
-    detalleTransfer?: DetalleTransferencia | null,
-    onChange?:  (e:React.ChangeEvent<HTMLTextAreaElement>)=>void
-    
+  isDisable?: boolean;
+  tipo: string;
+  detalleTransfer?: DetalleTransferencia | null;
+  onChange?: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
 }
 
-export const TextArea = ({ isDisable = false,tipo,detalleTransfer, onChange }: TextAreaProps) => {
-
-   /* const handleOnchange = (e: React.ChangeEvent<HTMLTextAreaElement>) => { 
+export const TextArea = ({
+  isDisable = false,
+  tipo,
+  detalleTransfer,
+  onChange,
+}: TextAreaProps) => {
+  /* const handleOnchange = (e: React.ChangeEvent<HTMLTextAreaElement>) => { 
         console.log(e)
        // setinputValidate(e.target.value)
     }*/
 
-    return (
-        <textarea className={`block w-full bg-gray-50 rounded-lg border border-gray-300 p-1 overflow-auto ${isDisable? "text-xs text-gray-500":"text-sm  text-gray-900"}`} name={tipo} id={tipo} rows={3} disabled={isDisable} value={detalleTransfer?.log_detalle} onChange={onChange}>
-        </textarea>
-    )
-}
-
+  return (
+    <textarea
+      rows={4}
+      className={`block w-full  rounded-lg border border-gray-300 p-1 overflow-auto ${
+        isDisable
+          ? "text-sm text-gray-500 bg-gray-100"
+          : "text-sm  text-gray-900"
+      }`}
+      name={tipo}
+      id={tipo}
+      disabled={isDisable}
+      value={detalleTransfer?.log_detalle}
+      onChange={onChange}
+    ></textarea>
+  );
+};
--- app-transfer/src/components/molecules/CardTexArea.tsx
+++ app-transfer/src/components/molecules/CardTexArea.tsx
@@ -1,26 +1,41 @@
-import { DetalleTransferencia } from "../../interfaces/DetalleTransferencia"
-import { TextArea } from "../atom/TextArea"
+import { DetalleTransferencia } from "../../interfaces/DetalleTransferencia";
+import { TextArea } from "../atom/TextArea";
 
 interface CardTextArea {
-    label: string,
-    isDisable?: boolean,
-    tipo:string,
-    detalleTransfer?: DetalleTransferencia | null,
-    onChange?:  (e:React.ChangeEvent<HTMLTextAreaElement>)=>void
-
-    
+  label: string;
+  isDisable?: boolean;
+  tipo: string;
+  detalleTransfer?: DetalleTransferencia | null;
+  onChange?: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
+  padding?: boolean;
 }
 
-export const CardTexArea = ({ label, isDisable=false,tipo,detalleTransfer,onChange }: CardTextArea) => {
-
-    
-    return (
-        <>
-            <div className="px-2">
-                < label htmlFor={tipo} className="block mb-2 text-sm font-medium text-gray-900 px-2 ">{label}</label>
-                <TextArea isDisable={isDisable}  tipo={tipo} detalleTransfer={detalleTransfer} onChange={onChange}/>
-            </div>
-        </>
-    )
-}
-
+export const CardTexArea = ({
+  label,
+  isDisable = false,
+  tipo,
+  detalleTransfer,
+  onChange,
+  padding = true,
+}: CardTextArea) => {
+  return (
+    <>
+      <div className={`${padding ? "px-2" : ""} mt-3`}>
+        <label
+          htmlFor={tipo}
+          className={`block mb-2 text-sm font-medium text-gray-900  ${
+            padding ? "px-2" : ""
+          }`}
+        >
+          {label}
+        </label>
+        <TextArea
+          isDisable={isDisable}
+          tipo={tipo}
+          detalleTransfer={detalleTransfer}
+          onChange={onChange}
+        />
+      </div>
+    </>
+  );
+};
