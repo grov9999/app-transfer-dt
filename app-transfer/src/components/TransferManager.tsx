701dac2758623f8ef7dc7c377bbbc22ddf80e559
Daniel <lequernaquegd@gmail.com>
2025-04-01 11:01:05 -0500

    UPDATE: cambios en el diseño

--- app-transfer/src/components/TransferManager.tsx
+++ app-transfer/src/components/TransferManager.tsx
@@ -125,34 +125,34 @@ export const TransferManager = () => {
                         </tr>
                     </tbody>
                 </table>
-                
-                <button type="button" className="px-5 py-2.5 text-sm font-medium text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-center">Aprobar</button>
-                <button type="button" className="px-5 py-2.5 text-sm font-medium text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-center">Rechazar</button>
-                <button type="button" className="px-5 py-2.5 text-sm font-medium text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-center">Consultar</button>
-
+                <div className="flex-wrap justify-center">
+                    <button type="button" className="px-5 py-2.5 text-sm font-medium text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-center">Aprobar</button>
+                    <button type="button" className="px-5 py-2.5 text-sm font-medium text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-center">Rechazar</button>
+                    <button type="button" className="px-5 py-2.5 text-sm font-medium text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-center">Consultar</button>
+                </div>
                 <nav className="flex items-center flex-column flex-wrap md:flex-row justify-between pt-4" aria-label="Table navigation">
-                    <span className="text-sm font-normal text-gray-500 dark:text-gray-400 mb-4 md:mb-0 block w-full md:inline md:w-auto">Showing <span className="font-semibold text-gray-900">1-10</span> of <span className="font-semibold text-gray-900">1000</span></span>
+                    <span className="text-sm font-normal text-gray-50 mb-4 md:mb-0 block w-full md:inline md:w-auto">Showing <span className="font-semibold text-gray-900">1-10</span> of <span className="font-semibold text-gray-900">1000</span></span>
                     <ul className="inline-flex -space-x-px rtl:space-x-reverse text-sm h-8">
                         <li>
-                            <a href="#" className="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Previous</a>
+                            <a href="#" className="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700">Previous</a>
                         </li>
                         <li>
-                            <a href="#" className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">1</a>
+                            <a href="#" className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700">1</a>
                         </li>
                         <li>
-                            <a href="#" className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">2</a>
+                            <a href="#" className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700">2</a>
                         </li>
                         <li>
-                            <a href="#" aria-current="page" className="flex items-center justify-center px-3 h-8 text-blue-600 border border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white">3</a>
+                            <a href="#" aria-current="page" className="flex items-center justify-center px-3 h-8 text-blue-600 border border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700">3</a>
                         </li>
                         <li>
-                            <a href="#" className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">4</a>
+                            <a href="#" className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700">4</a>
                         </li>
                         <li>
-                            <a href="#" className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">5</a>
+                            <a href="#" className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700">5</a>
                         </li>
                         <li>
-                            <a href="#" className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Next</a>
+                            <a href="#" className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700">Next</a>
                         </li>
                     </ul>
                 </nav>
--- app-transfer/src/components/TransferSearch.tsx
+++ app-transfer/src/components/TransferSearch.tsx
@@ -2,23 +2,22 @@ export const TransferSearch = () => {
   return (
     <form>
       <div className="flex justify-between">
-        <h1 className="">Gestor de Transferencia DT</h1>
-        <h1 className="text-2xl text-gray-90">Gestor de Transferencia DT</h1>
+        <h1 className="text-2xl text-gray-90 ">Gestor de Transferencia DT</h1>
         <button type="button" className="px-5 py-2.5 text-sm font-medium text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-center">+ Nueva Parte</button>
       </div>
       <div className="mx-auto bg-white p-6 rounded-lg shadow-md">
-        <h1>Buscar Partes</h1>
+        <h1 className="text-xl text-gray-90">Buscar Partes</h1>
         <div className="grid gap-6 mb-6 md:grid-cols-4">
           <div>
             <label
-              htmlFor="first_name"
+              htmlFor="ptcode"
               className="block mb-2 text-sm font-medium text-gray-900 "
             >
               CÓDIGO PT
             </label>
             <input
               type="text"
-              id="first_name"
+              id="ptcode"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
               placeholder="Buscar"
               required
@@ -26,14 +25,14 @@ export const TransferSearch = () => {
           </div>
           <div>
             <label
-              htmlFor="last_name"
+              htmlFor="status"
               className="block mb-2 text-sm font-medium text-gray-900 "
             >
               ESTADO
             </label>
             <input
               type="text"
-              id="last_name"
+              id="status"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
               placeholder="Todos"
               required
@@ -41,14 +40,14 @@ export const TransferSearch = () => {
           </div>
           <div>
             <label
-              htmlFor="company"
+              htmlFor="costcenter"
               className="block mb-2 text-sm font-medium text-gray-900 "
             >
               CENTRO DE COSTO
             </label>
             <input
               type="text"
-              id="company"
+              id="costcenter"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
               placeholder="Todos"
               required
@@ -56,14 +55,14 @@ export const TransferSearch = () => {
           </div>
           <div>
             <label
-              htmlFor="phone"
+              htmlFor="datefrom"
               className="block mb-2 text-sm font-medium text-gray-900 "
             >
               FECHA DESDE
             </label>
             <input
               type="tel"
-              id="phone"
+              id="datefrom"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
               placeholder="DD/MM/AAAA"
               pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
@@ -72,14 +71,14 @@ export const TransferSearch = () => {
           </div>
           <div>
             <label
-              htmlFor="website"
+              htmlFor="dateuntil"
               className="block mb-2 text-sm font-medium text-gray-900 "
             >
               FECHA HASTA
             </label>
             <input
               type="url"
-              id="website"
+              id="dateuntil"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
               placeholder="DD/MM/AAAA"
               required
@@ -87,14 +86,14 @@ export const TransferSearch = () => {
           </div>
           <div>
             <label
-              htmlFor="visitors"
+              htmlFor="amountfrom"
               className="block mb-2 text-sm font-medium text-gray-900 "
             >
               MONTO DESDE
             </label>
             <input
               type="url"
-              id="website"
+              id="amountfrom"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
               placeholder="0.00"
               required
@@ -102,18 +101,22 @@ export const TransferSearch = () => {
           </div>
           <div>
             <label
-              htmlFor="visitors"
+              htmlFor="amountuntil"
               className="block mb-2 text-sm font-medium text-gray-900 "
             >
               MONTO HASTA
             </label>
             <input
               type="url"
-              id="website"
+              id="amountuntil"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
               placeholder="0.00"
               required
             />
+          </div>
+          <div className="flex-auto justify-center">
+            <button type="button" className="px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">Buscar</button>
+            <button type="button" className="px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">Limpiar</button>
           </div>
         </div>
       </div>
